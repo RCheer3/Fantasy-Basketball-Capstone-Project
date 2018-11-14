8c0fdcc4de757e697957a16173856d4f742c6b49
Robert Cheer <rcheer@Roberts-MacBook-Pro.local>
2018-11-13 17:14:56 -0800

    EOD 11/13 commit

---
+++ Test code/Evaluating Ranks.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def evaluate(l1,l2,n=150):
+    for i in range(1,n+1):
+        a = np.array(l1.copy()[0:i])
+        b = 0.0
+        for x in range(0,i):
+            if l2.copy().loc[x] in a:
+                b+=1.0
+        weights.append(b/i)
+    return np.mean(weights)
+
